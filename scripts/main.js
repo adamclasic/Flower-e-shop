519bcda815991e1a699df5359f2f65bc6a6fe2c4
adam allalou <aabou9@gmail.com>
2020-03-15 20:52:31 +0100

    add footer

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,46 @@
+function addOrRemove(array, value) {
+    var index = array.indexOf(value);
+
+    if (index === -1) {
+        array.push(value);
+    } else {
+        array.splice(index, 1);
+    }
+}
+
+function addToFav(pId, arr) {
+
+    $('#' + pId).click(function() {
+        addOrRemove(arr, pId);
+        if (arr.length == 0) {
+            $("#fav-num").addClass('d-none');
+        } else {
+            $("#fav-num").removeClass('d-none');
+        }
+        $("#fav-num").text(arr.length);
+        $('#' + pId).toggleClass("fas far");
+    });
+}
+
 $(document).ready(function () {
+
+    var arr = [];
 
     $(window).scroll(function(){
         $('#header').toggleClass('bg-green', $(this).scrollTop() > 500);
     });
+
+    addToFav('product1', arr);
+    addToFav('product2', arr);
+    addToFav('product3', arr);
+    addToFav('product4', arr);
+    addToFav('product5', arr);
+    addToFav('product6', arr);
+
+    
+
+
+
 
 
 });
