8af5c6b8ca7dd3dd221f26e75e311145347397d8
adam allalou <aabou9@gmail.com>
2020-03-16 18:10:45 +0100

    add checkout file

--- scripts/main.js
+++ scripts/main.js
@@ -37,10 +37,25 @@ $(document).ready(function () {
     addToFav('product5', arr);
     addToFav('product6', arr);
 
-    
-
-
-
-
+    // var slider = document.getElementById('slider');
 
 });
+
+$( function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#smallRange" ).val("$" + ui.values[ 0 ]);
+        $( "#bigRange" ).val("$" + ui.values[ 1 ]) ;
+      }
+    });
+      
+    $('#smallRange').val("$" +  $("#slider-range" ).slider( "values", 0 ));  
+    $('#bigRange').val("$" +  $("#slider-range" ).slider( "values", 1 ));  
+  } );
+
+
+
