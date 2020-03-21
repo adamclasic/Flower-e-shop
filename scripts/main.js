1c193af290cc9018cb88c2af0a9fb1839a2dd67f
adam allalou <aabou9@gmail.com>
2020-03-21 09:01:29 +0100

    update filter checkout

--- scripts/main.js
+++ scripts/main.js
@@ -29,22 +29,32 @@ function addToFav(pId, arr) {
         $("#fav-num").text(arr.length);
         $('#' + pId).toggleClass("fas far");
     });
-}
-
-var swiper = new Swiper('.swiper-container', {
-    // slidesPerView: 2,
-    spaceBetween: 10,
-    centeredSlides: true,
-    width: 300,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  });
-
+};
 
 
 $(document).ready(function () {
+
+    if ($(window).width() < 700) {
+        $('#grid-3').addClass('swiper-wrapper');
+        $('#grid-3').removeClass('grid-wraper-3');
+        $('#grid-3-cont').addClass('swiper-container');
+        $('.card-2').addClass('swiper-slide');
+    } else {
+        $('#grid-3').removeClass('swiper-wrapper');
+        $('#grid-3').addClass('grid-wraper-3');
+        $('#grid-3-cont').removeClass('swiper-container');
+        $('.card-2').removeClass('swiper-slide');
+    }
+    
+    var swiper = new Swiper('.swiper-container', {
+        spaceBetween: 15,
+        centeredSlides: true,
+        width: 260,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+    });
 
     var arr = [];
 
@@ -59,9 +69,7 @@ $(document).ready(function () {
     addToFav('product5', arr);
     addToFav('product6', arr);
 
-    // var slider = document.getElementById('slider');
-
-    // $(window).resize(function() {
+    $(window).resize(function() {
         
         if ($(window).width() < 700) {
             $('#grid-3').addClass('swiper-wrapper');
@@ -84,25 +92,28 @@ $(document).ready(function () {
               el: '.swiper-pagination',
               clickable: true,
             },
-          });
+        });
         
-    // });
-    })
+    });
 
-    // var mq = window.matchMedia("(max-width: 1050px)");
-    // WidthChange(mq);
-    // // every time width changes, check the media query
-    // mq.addListener(function WidthChange(mq){
-    //    if(mq.matches){
-    //        //we are in a mobile size browser
-    //        $('#menu3').addClass('bg-green');
-    //        $('#menu3').removeClass('grid-wraper-3');
-    //    } else{
-    //        // desktop browser
-    //        $('#menu3').addClass('grid-wraper-3');
-    //        $('#menu3').removeClass('bg-green');
-    //    }
-    // });
+    $( function() {
+        $( ".slider-range" ).slider({
+          range: true,
+          min: 0,
+          max: 500,
+          values: [ 75, 300 ],
+          slide: function( event, ui ) {
+            $( ".smallRange" ).val("$" + ui.values[ 0 ]);
+            $( ".bigRange" ).val("$" + ui.values[ 1 ]) ;
+          }
+        });
+          
+        $('.smallRange').val("$" +  $(".slider-range" ).slider( "values", 0 ));  
+        $('.bigRange').val("$" +  $(".slider-range" ).slider( "values", 1 ));  
+    });
+    
+
+
 
     $(window).resize(function() {
         if(false == $('#grid-3').hasClass('swiper-wrapper') && $(window).width() <= 700) {
@@ -117,43 +128,16 @@ $(document).ready(function () {
           $('#grid-3').addClass('grid-wraper-3');
           $('#grid-3-cont').removeClass('swiper-container');
           $('.card-2').removeClass('swiper-slide');
-
-
-
         }
 
         var swiper = new Swiper('.swiper-container', {
-            // slidesPerView: 2,
             spaceBetween: 15,
             centeredSlides: true,
             width: 260,
-            // pagination: {
-            //   el: '.swiper-pagination',
-            //   clickable: true,
-            // },
-          });
-          
-      });
-
-      
-
-// });
-
-$( function() {
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 500,
-      values: [ 75, 300 ],
-      slide: function( event, ui ) {
-        $( "#smallRange" ).val("$" + ui.values[ 0 ]);
-        $( "#bigRange" ).val("$" + ui.values[ 1 ]) ;
-      }
+        });
     });
+
+});
+
+          
       
-    $('#smallRange').val("$" +  $("#slider-range" ).slider( "values", 0 ));  
-    $('#bigRange').val("$" +  $("#slider-range" ).slider( "values", 1 ));  
-  } );
-
-
-
